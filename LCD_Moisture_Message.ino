b605159ce61f1abcb511fddb968515c7b23c151c
Jakub Jadwiszczak <jadwiszj@tcd.ie>
2020-08-02 13:43:44 -0400

    Update LCD_Moisture_Message.ino
    changed variable and added proper map range for moisture.

--- LCD_Moisture_Message.ino
+++ LCD_Moisture_Message.ino
@@ -3,8 +3,8 @@
 // initialize the library by associating any needed LCD interface pin
 // with the Arduino pin number it is connected to:
 
-const int dry = 870; // value on cap sensor when it is fully dry.
-const int wet = 514; // value on cap sensor when it is fully wet.
+const int wet = 292; // value on cap sensor when it is fully wet.
+const int dry = 531; // value on cap sensor when it is fully dry.
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -23,7 +23,7 @@ void setup() {
 void loop() {
   //lcd.setCursor(0, 1);
   //lcd.print("THANKS");
-  int trip_pin = 8;
+  int trip_pin = A0;
   int val = analogRead(trip_pin); // initialize the read value for the moisture input pin.
   Serial.println(val);
 
@@ -36,20 +36,20 @@ void displayMoisture() {
     int sensorValue;
     sensorValue = analogRead(A0);
     int percentageHumidity = map(sensorValue, wet, dry, 100, 0);   // convert sensor reading to voltage  
-    phC = percentageHumidity;
+   
   
     Serial.println("Cap value on moisture sensor is: ");  
-    Serial.print(phC); Serial.println("%");
+    Serial.print(percentageHumidity); Serial.println("%");
     
-    if(phC < 10) {
+    if(percentageHumidity < 10) {
       
-      Serial.print("Signal has tripped at "); Serial.print(m_voltage); Serial.println(" Volts."); 
+      Serial.print("Signal has tripped at "); Serial.print(percentageHumidity); Serial.println(" cap percentage."); 
       // print the read-out value at the moment of loop trip to the Serial Monitor.
       lcd.clear();
       lcd.print("Getting thirsty..."); // change the displayed text on the LCD screen.
       delay(3000);
     }
-      else if(phC > 10) {
+      else if(percentageHumidity > 10) {
         
       lcd.clear();
       lcd.print("I'm happy ^^");
